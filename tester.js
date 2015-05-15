c7f28f0c7166e1065f76f608a00d16a5619b703e
earthquakefounder <earthquakefounder@gmail.com>
2015-05-14 21:31:21 -0400

    More and more changes!

--- tester.js
+++ tester.js
@@ -1,5 +1,4 @@
 /// <reference path="typed/jquery.d.ts" />
-var Testing;
 (function (Testing) {
     var $element = $('body');
     Object.defineProperty(Testing, 'Element', {
@@ -10,10 +9,109 @@ var Testing;
             $element = $(element);
         }
     });
-    var Tester = (function () {
-        function Tester(testFn) {
-        }
-        return Tester;
-    })();
+
+    function Tester(name, testFn) {
+        var _this = this;
+        var controller = new TestController($element, testFn);
+
+        return function () {
+            var args = [];
+            for (var _i = 0; _i < (arguments.length - 0); _i++) {
+                args[_i] = arguments[_i + 0];
+            }
+            controller.addTest(_this, args);
+        };
+    }
     Testing.Tester = Tester;
-})(Testing = exports.Testing || (exports.Testing = {}));
+
+    var testHtml = [
+        '<div class="test">',
+        '<div class="test-name"></div>',
+        '<div class="row test-headers">',
+        '<div class="test-input">Input</div>',
+        '<div class="test-output">Output</div>',
+        '<div class="test-result">Result</div>',
+        '</div>',
+        '</div>'
+    ].join(''), resultHtml = [
+        '<div class="row test-result">',
+        '<div class="test-input"></div>',
+        '<div class="test-output"></div>',
+        '<div class="test-result"></div>',
+        '</div>'
+    ].join('');
+
+    var TestController = (function () {
+        function TestController(appendTo, testFn) {
+            this.$test = $(testHtml).appendTo(appendTo);
+            this.fn = testFn;
+        }
+        TestController.prototype.addTest = function (context) {
+            var inputs = [];
+            for (var _i = 0; _i < (arguments.length - 1); _i++) {
+                inputs[_i] = arguments[_i + 1];
+            }
+        };
+        return TestController;
+    })();
+
+    var Result = (function () {
+        function Result() {
+            this._r = true;
+            this._ready = false;
+        }
+        Object.defineProperty(Result.prototype, "result", {
+            get: function () {
+                return this._r;
+            },
+            set: function (r) {
+                this._r = r;
+                this._ready = true;
+            },
+            enumerable: true,
+            configurable: true
+        });
+
+        Result.prototype.shouldBe = function (value) {
+            var _this = this;
+            return this.check(function () {
+                return _this.result === value;
+            }, 'Expected: ${value}, Got: ${this.result}');
+        };
+
+        Result.prototype.shouldBeTrue = function () {
+            return this.shouldBe(true);
+        };
+
+        Result.prototype.shouldBeFalse = function () {
+            return this.shouldBe(false);
+        };
+
+        Result.prototype.shouldBeTruthy = function () {
+            return this;
+        };
+
+        Result.prototype.shouldBeFalsy = function () {
+            return this;
+        };
+
+        Result.prototype.shouldThrowException = function (text) {
+            return this;
+        };
+
+        Result.prototype.check = function (callback, error) {
+            if (!this._ready) {
+                this._checkStack.push(callback);
+            } else {
+                setTimeout(function () {
+                    if (callback()) {
+                    }
+                }, 0);
+            }
+
+            return this;
+        };
+        return Result;
+    })();
+})(exports.Testing || (exports.Testing = {}));
+var Testing = exports.Testing;
--- tester.ts
+++ tester.ts
@@ -11,10 +11,113 @@ export module Testing {
 		}
 	})
 	
-	export interface IShouldable {
-		shouldBe: function(...args: any[])
+	export interface IResultChecker {
+		shouldBe: (value: any) => IResultChecker;
+		shouldBeTrue: () => IResultChecker;
+		shouldBeFalse: () => IResultChecker;
+		shouldBeTruthy: () => IResultChecker;
+		shouldBeFalsy: () => IResultChecker;
+		shouldThrowException: (text? : string) => IResultChecker;
 	}
-	export function Tester(testFn: (...args : any[]) => any) {
+	
+	export function Tester(name: string, testFn: (...args : any[]) => any) {
+		var controller = new TestController($element, testFn);
 		
+		return (...args : any[]) => {
+			controller.addTest(this, args);
+		};
+	}
+	
+	var testHtml = [
+		'<div class="test">',
+			'<div class="test-name"></div>',
+			'<div class="row test-headers">',
+				'<div class="test-input">Input</div>',
+				'<div class="test-output">Output</div>',
+				'<div class="test-result">Result</div>',
+			'</div>',
+		'</div>'
+	].join(''),
+	resultHtml = [
+		'<div class="row test-result">',
+			'<div class="test-input"></div>',
+			'<div class="test-output"></div>',
+			'<div class="test-result"></div>',
+		'</div>'
+	].join('');
+	
+	class TestController {
+		private $test: JQuery;
+		private fn: (...args: any[]) => any;
+		
+		constructor(appendTo: JQuery, testFn: (...args: any[]) => any) {
+			this.$test = $(testHtml).appendTo(appendTo);
+			this.fn = testFn;
+		}
+		
+		addTest(context: any, ...inputs: any[]) {
+			
+		}
+	}
+	
+	class Result implements IResultChecker {
+		private _r: any = true;
+		private _ready: boolean = false;
+		private _checkStack: Array<{callback: () => boolean, error?: string}>;
+		
+		get result(): any {
+			return this._r;
+		}
+		set result(r) {
+			this._r = r;
+			this._ready = true;
+			
+			for(var i = 0; i < this._checkStack.length; i++) {
+				this.check(this._checkStack[i].callback, this._checkStack[i].error);
+			}
+		}
+		
+		shouldBe(value: any) {
+			return this.check(() => { 
+				return this.result === value;
+			}, 'Expected: ' + value + ' Got: ' + this.result);
+		}
+		
+		shouldBeTrue() {
+			return this.shouldBe(true);
+		}
+		
+		shouldBeFalse() {
+			return this.shouldBe(false);
+		}
+		
+		shouldBeTruthy() {
+			return this;
+		}
+		
+		shouldBeFalsy() {
+			return this;
+		}
+		
+		shouldThrowException(text? : string) {
+			return this;	
+		}
+		
+		private check(callback: () => boolean, error?: string) {
+			if(!this._ready) {
+				this._checkStack.push({
+					callback: callback,
+					error: error
+				});
+			} else {
+				setTimeout(() => {
+					if(callback()) {
+						
+					} 
+				}, 0);
+			}
+			
+			return this;
+		}
 	}
 }
